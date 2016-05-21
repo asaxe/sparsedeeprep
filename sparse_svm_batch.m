78bcf0636dbb5d905f7f16970260ce9214933af1
Andrew Saxe <asaxe@fas.harvard.edu>
2016-05-22 00:22:09 +0300

    multiple dataset sizes, large test set

--- sparse_svm_batch.m
+++ sparse_svm_batch.m
@@ -4,71 +4,75 @@ addpath('libsvm-compact-0.1/matlab')
 
 %% Generate dataset
 
-Ns = 50; % Input dimension
-P = 50; % Number of centroids
-Nepochs = 500; % Number of epochs of training data to generate for overall batch. Total # of training samples in batch is P*Nepochs
-dS = .05; % Probability of noise corruption
-Ncs = [1000]; % Number of random hidden layer filters
+for Nepochs = [10 100 200]
 
-c = rand(Ns,P)>.5; % Centroid locations
-y = 2*((rand(1,P)>.5)-1/2); % Centroid labels
+    Ns = 50; % Input dimension
+    P = 50; % Number of centroids
+    fprintf('Num examples = %d',P*Nepochs) % = 10; % Number of epochs of training data to generate for overall batch. Total # of training samples in batch is P*Nepochs
+    Nepochs_test = 500;
+    dS = .05; % Probability of noise corruption
+    Ncs = [1000]; % Number of random hidden layer filters
 
-x = (2*(repmat(c,1,Nepochs)-1/2).*(2*((rand(Ns,P*Nepochs) >= (dS/2)) - 1/2)))/2+1/2;
-y = repmat(y,1,Nepochs);
+    c = rand(Ns,P)>.5; % Centroid locations
+    y = 2*((rand(1,P)>.5)-1/2); % Centroid labels
 
-xt = (2*(repmat(c,1,Nepochs)-1/2).*(2*((rand(Ns,P*Nepochs) >= (dS/2)) - 1/2)))/2+1/2;
-yt = y;
+    x = (2*(repmat(c,1,Nepochs)-1/2).*(2*((rand(Ns,P*Nepochs) >= (dS/2)) - 1/2)))/2+1/2;
+    y = repmat(y,1,Nepochs);
 
-%% Train SVM
+    xt = (2*(repmat(c,1,Nepochs_test)-1/2).*(2*((rand(Ns,P*Nepochs_test) >= (dS/2)) - 1/2)))/2+1/2;
+    yt = y;
 
-trn = single(x);
-trn_lab = y;
+    %% Train SVM
 
-tst = single(xt);
-tst_lab = yt;
+    trn = single(x);
+    trn_lab = y;
 
-clear x xt y yt;
+    tst = single(xt);
+    tst_lab = yt;
 
-m = mean(trn(:)); trn = trn - m; tst = tst - m;
+    clear x xt y yt;
+
+    m = mean(trn(:)); trn = trn - m; tst = tst - m;
 
 
-trn = bsxfun(@rdivide, trn, sqrt(sum(trn.^2)));
-tst = bsxfun(@rdivide, tst, sqrt(sum(tst.^2)));
+    trn = bsxfun(@rdivide, trn, sqrt(sum(trn.^2)));
+    tst = bsxfun(@rdivide, tst, sqrt(sum(tst.^2)));
 
-clip = @(K)max(min(K,1),-1);
+    clip = @(K)max(min(K,1),-1);
 
-ssrelu = @(K)(sqrt(1-K.^2) + (pi-acos(K)).*K)/pi;
-tsrelu = @(K)(1 - acos(K)/pi).*K;
+    ssrelu = @(K)(sqrt(1-K.^2) + (pi-acos(K)).*K)/pi;
+    tsrelu = @(K)(1 - acos(K)/pi).*K;
 
-func = { ssrelu ,  tsrelu };
-name = {'SSReLU', 'TSReLU'};
+    func = { ssrelu ,  tsrelu };
+    name = {'SSReLU', 'TSReLU'};
 
-for c = 10.^[1]
-    
-    for f = [2 1] %{tsrelu, ssrelu}
-
-        ktrn = zeros(size(trn,2)+1, size(trn,2), 'single'); ktrn(1,:) = 1:size(trn,2);
-        ktst = zeros(size(trn,2)+1, size(tst,2), 'single'); ktst(1,:) = 1:size(tst,2);
-
-        ktrn(2:end,:) = clip(trn' * trn);
-        ktst(2:end,:) = clip(tst' * tst);
-
-        acc = 0;
+    for c = 10.^[1]
         
-        for l = 1:100
+        for f = [2 1] %{tsrelu, ssrelu}
 
-            fprintf('%s: c=%.1e, l=%02d, ', name{f}, c, l);
+            ktrn = zeros(size(trn,2)+1, size(trn,2), 'single'); ktrn(1,:) = 1:size(trn,2);
+            ktst = zeros(size(trn,2)+1, size(tst,2), 'single'); ktst(1,:) = 1:size(tst,2);
 
-            ktrn(2:end,:) = clip(func{f}(ktrn(2:end,:)));
-            ktst(2:end,:) = clip(func{f}(ktst(2:end,:)));
+            ktrn(2:end,:) = clip(trn' * trn);
+            ktst(2:end,:) = clip(tst' * tst);
 
-            [~, res, ~] = svmpredict_inplace(tst_lab, ktst, svmtrain_inplace(trn_lab, ktrn, sprintf('-t 4 -c %d -q', c)));
+            acc = 0;
             
-            if res(1) > acc, acc = res(1); else break; end % auto stop
+            for l = 1:100
 
+                fprintf('%s: c=%.1e, l=%02d, ', name{f}, c, l);
+
+                ktrn(2:end,:) = clip(func{f}(ktrn(2:end,:)));
+                ktst(2:end,:) = clip(func{f}(ktst(2:end,:)));
+
+                [~, res, ~] = svmpredict_inplace(tst_lab, ktst, svmtrain_inplace(trn_lab, ktrn, sprintf('-t 4 -c %d -q', c)));
+                
+                if res(1) > acc, acc = res(1); else break; end % auto stop
+
+            end
+            
+            fprintf('\n');
         end
-        
-        fprintf('\n');
     end
-end
 
+end
